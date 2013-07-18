48101b047c130b541aa12660227c0c04ef25c0b0
j105rob <j105.rob@gmail.com>
2013-07-18 10:02:31 -0400

    update

--- detect.py
+++ detect.py
@@ -12,6 +12,7 @@ class DeviceAddedListener:
         self.hal_manager = dbus.Interface(self.hal_manager_obj,
                                           "org.freedesktop.Hal.Manager")
         self.hal_manager.connect_to_signal("DeviceAdded", self._filter)
+        self.hal_manager.connect_to_signal("DeviceRemoved", self._removal)
         
     def _filter(self, udi):
         device_obj = self.bus.get_object ("org.freedesktop.Hal", udi)
@@ -20,58 +21,70 @@ class DeviceAddedListener:
         if device.QueryCapability("volume"):
             return self.do_something(device)
         
+    def _removal(self,udi):
+        #device_obj = self.bus.get_object ("org.freedesktop.Hal", udi)
+        #device = dbus.Interface(device_obj, "org.freedesktop.Hal.Device")
+        if self.mounted:
+            result = commands.getstatusoutput('sudo umount -f %s' % (self.mount_dir))
+            result = commands.getstatusoutput('sudo rm -rf %s' % (self.mount_dir))
+            print "Cleaned up..."
+            self.mounted = False
+        #if device.QueryCapability("volume"):
+        #    print "USB removed..."
+                    
     def do_something(self, volume):
-        print dir(volume)
-        device_file = volume.GetProperty("block.device")
-        label = volume.GetProperty("volume.label")
-        fstype = volume.GetProperty("volume.fstype")
-        mounted = volume.GetProperty("volume.is_mounted")
-        mount_point = volume.GetProperty("volume.mount_point")
-        uuid = volume.GetProperty("volume.uuid")
+        #print dir(volume)
+        self.device_file = volume.GetProperty("block.device")
+        self.label = volume.GetProperty("volume.label")
+        self.fstype = volume.GetProperty("volume.fstype")
+        self.mounted = volume.GetProperty("volume.is_mounted")
+        self.mount_point = volume.GetProperty("volume.mount_point")
+        self.uuid = volume.GetProperty("volume.uuid")
         try:
             size = volume.GetProperty("volume.size")
         except:
             size = 0
 
         print "New storage device detected:"
-        print "  device_file: %s" % device_file
-        print "  UUID: %s"%uuid
-        print "  label: %s" % label
-        print "  fstype: %s" % fstype
-        if mounted:
+        print "  device_file: %s" % self.device_file
+        print "  UUID: %s"%self.uuid
+        print "  label: %s" % self.label
+        print "  fstype: %s" % self.fstype
+        if self.mounted:
             print "  mount_point: %s" % mount_point
         else:
             print "  not mounted"
         print "  size: %s (%.2fGB)" % (size, float(size) / 1024**3)
-        mount_dir = "/mnt/" + label
-        mkdir = 'sudo mkdir %s' % (mount_dir)
-        mount = 'sudo mount -U %s %s' % (uuid, mount_dir)
-        print 'Mounting device: %s (%s)' % (label, mount_dir)
-        if not os.path.exists(mount_dir):
+        self.mount_dir = "/mnt/" + self.label
+        mkdir = 'sudo mkdir %s' % (self.mount_dir)
+        mount = 'sudo mount -U %s %s' % (self.uuid, self.mount_dir)
+        print 'Mounting device: %s (%s)' % (self.label, self.mount_dir)
+        if not os.path.exists(self.mount_dir):
             result = commands.getstatusoutput(mkdir)
             if result[0] != 0:
                 print 'dir creation failed, aborting USB mount...'
                 sys.exit(1)
             else:
                 result = commands.getstatusoutput(mount)
+                self.mounted = True
         else:
             print 'unclean mount directory, please clean up...'
             sys.exit(1)
-        time.sleep(3)
-        cp = "sudo cp ./payload.txt /mnt/"+label+"/payload.txt"
+        #time.sleep(3)
+        cp = "sudo cp ./payload.txt /mnt/"+self.label+"/payload.txt"
         result = commands.getstatusoutput(cp)
         if result[0] != 0:
            print 'Filed to copy file...'
            sys.exit(1)
         else:
             print "Copied file..."
-        result = commands.getstatusoutput('sudo umount %s' % (mount_dir))
-        if result[1] == 'umount: %s: not mounted' % (mount_dir):
-            commands.getstatusoutput('sudo rm -rf %s' % (mount_dir))
-        print 'sudo rm -rf %s' % (mount_dir)
-        result = commands.getstatusoutput('sudo rm -rf %s' % (mount_dir))
-        print "Cleaning up..."
-        print result
+        #result = commands.getstatusoutput('sudo umount -f %s' % (mount_dir))
+        #if result[1] == 'umount: %s: not mounted' % (mount_dir):
+        #    commands.getstatusoutput('sudo rm -rf %s' % (mount_dir))
+        #print 'sudo rm -rf %s' % (mount_dir)
+        #result = commands.getstatusoutput('sudo rm -rf %s' % (mount_dir))
+        #print "Cleaning up..."
+        #print result
         
 
 if __name__ == '__main__':
