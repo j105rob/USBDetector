fb2045e3fa1b2cc98843ae7c65ee5363f7e7d038
Tyler Mikesell <tmikesell17@gmail.com>
2014-07-16 15:04:33 -0400

    updated code
    fixed the mounting/unmounting. still need to make it only copy 1 egg
    instead of entire list of eggs over.

--- detect.py
+++ detect.py
@@ -1,6 +1,6 @@
 import dbus
 import gobject
-import commands,os,sys
+import commands,os,sys,random
 import time
 
 class DeviceAddedListener:
@@ -22,18 +22,17 @@ class DeviceAddedListener:
             return self.do_something(device)
         
     def _removal(self,udi):
-        #device_obj = self.bus.get_object ("org.freedesktop.Hal", udi)
-        #device = dbus.Interface(device_obj, "org.freedesktop.Hal.Device")
+
         if self.mounted:
             result = commands.getstatusoutput('sudo umount -f %s' % (self.mount_dir))
             result = commands.getstatusoutput('sudo rm -rf %s' % (self.mount_dir))
-            print "Cleaned up..."
+            print "Jobs Done."
             self.mounted = False
-        #if device.QueryCapability("volume"):
-        #    print "USB removed..."
                     
     def do_something(self, volume):
-        #print dir(volume)
+        """
+        sets all the properties of the drive.
+        """
         self.device_file = volume.GetProperty("block.device")
         self.label = volume.GetProperty("volume.label")
         self.fstype = volume.GetProperty("volume.fstype")
@@ -55,7 +54,7 @@ class DeviceAddedListener:
         else:
             print "  not mounted"
         print "  size: %s (%.2fGB)" % (size, float(size) / 1024**3)
-        self.mount_dir = "/mnt/" + self.label
+        self.mount_dir = "/median/" + self.label
         mkdir = 'sudo mkdir %s' % (self.mount_dir)
         mount = 'sudo mount -U %s %s' % (self.uuid, self.mount_dir)
         print 'Mounting device: %s (%s)' % (self.label, self.mount_dir)
@@ -70,22 +69,18 @@ class DeviceAddedListener:
         else:
             print 'unclean mount directory, please clean up...'
             sys.exit(1)
-        #time.sleep(3)
-        cp = "sudo cp ./payload.txt /mnt/"+self.label+"/payload.txt"
+        cp = "sudo cp ./payload.txt /media/"+self.label+"/payload.txt"
         result = commands.getstatusoutput(cp)
         if result[0] != 0:
-           print 'Filed to copy file...'
+           print 'Failed to copy file...'
            sys.exit(1)
         else:
             print "Copied file..."
-        #result = commands.getstatusoutput('sudo umount -f %s' % (mount_dir))
-        #if result[1] == 'umount: %s: not mounted' % (mount_dir):
-        #    commands.getstatusoutput('sudo rm -rf %s' % (mount_dir))
-        #print 'sudo rm -rf %s' % (mount_dir)
-        #result = commands.getstatusoutput('sudo rm -rf %s' % (mount_dir))
-        #print "Cleaning up..."
-        #print result
-        
+        result = commands.getstatusoutput('sudo umount -f %s' % (self.mount_dir))
+        if result[1] == 'umount: %s: not mounted' % (self.mount_dir):
+            commands.getstatusoutput('sudo rm -rf %s' % (self.mount_dir))
+        result = commands.getstatusoutput('sudo rm -rf %s' % (self.mount_dir))
+        print "Cleaning up..."        
 
 if __name__ == '__main__':
     from dbus.mainloop.glib import DBusGMainLoop
