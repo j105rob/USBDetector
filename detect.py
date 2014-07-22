98d8952985ac906b85b6552df66b996504156c44
Tyler Mikesell <tmikesell17@gmail.com>
2014-07-22 10:00:08 -0400

    Added LED support
    changed how it mounts/unmounts slightly to be more efficient. Now
    supports LEDs during processing a USB drive

--- detect.py
+++ detect.py
@@ -1,3 +1,4 @@
+import Adafruit_BBIO.GPIO as GPIO
 import dbus
 import gobject
 import commands,os,sys,random
@@ -5,14 +6,17 @@ import time
 
 class DeviceAddedListener:
     
+    GPIO.setup("P8_10", GPIO.OUT)
+    GPIO.setup("P8_14", GPIO.OUT)
+    GPIO.setup("P8_18", GPIO.OUT)
+
+    GPIO.output("P8_10", GPIO.HIGH)
+    
     def __init__(self):
         
         self.bus = dbus.SystemBus()
-        self.hal_manager_obj = self.bus.get_object(
-                                              "org.freedesktop.Hal",
-                                              "/org/freedesktop/Hal/Manager")
-        self.hal_manager = dbus.Interface(self.hal_manager_obj,
-                                          "org.freedesktop.Hal.Manager")
+        self.hal_manager_obj = self.bus.get_object("org.freedesktop.Hal", "/org/freedesktop/Hal/Manager")
+        self.hal_manager = dbus.Interface(self.hal_manager_obj, "org.freedesktop.Hal.Manager")
         self.hal_manager.connect_to_signal("DeviceAdded", self._filter)
         self.hal_manager.connect_to_signal("DeviceRemoved", self._removal)
 
@@ -30,75 +34,116 @@ class DeviceAddedListener:
 
         if self.usb:
             
+            GPIO.output("P8_10", GPIO.LOW)
+            GPIO.output("P8_18", GPIO.HIGH)
+            
             if self.mounted:
-                result = commands.getstatusoutput('sudo umount -f %s' % (self.mount_dir))
+                result = commands.getstatusoutput('sudo umount -f /dev/sda1')
                 result = commands.getstatusoutput('sudo rm -rf %s' % (self.mount_dir))
                 print "Device Removed."
 
             else:
                 print "Device removed, something was wrong with it."
+                
             self.mounted = False
             self.usb = False
+            
+            time.sleep(60)
+            
+            GPIO.output("P8_18", GPIO.LOW)
+            GPIO.output("P8_10", GPIO.HIGH)
+            print "Ready"
 
     def do_something(self, volume):
+        
+        GPIO.output("P8_10", GPIO.LOW)
+        GPIO.output("P8_14", GPIO.HIGH)
         
         self.device_file = volume.GetProperty("block.device")
         self.label = volume.GetProperty("volume.label")
         self.fstype = volume.GetProperty("volume.fstype")
         self.mounted = volume.GetProperty("volume.is_mounted")
         self.mount_point = volume.GetProperty("volume.mount_point")
-        self.uuid = volume.GetProperty("volume.uuid")
+        self.uuid = volume.GetProperty("volume.uuid")                                   
         
         try:
             size = volume.GetProperty("volume.size")
         except:
             size = 0
-
+                                                  
         print "New storage device detected:"
         print "  Device File: %s" % self.device_file
-        print "  UUID: %s"%self.uuid
+        print "  UUID: %s" % self.uuid
         print "  Label: %s" % self.label
         print "  Fstype: %s" % self.fstype
         print "  Size: %s (%.2fGB)" % (size, float(size) / 1024**3)
 
+        time.sleep(1)
+        
+        if ' ' in self.label:
+            name = self.label.split(' ')
+            self.label = name[0] + "\ " + name[1]
+        
         self.mount_dir = "/mnt/" + self.label
         mkdir = 'sudo mkdir %s' % (self.mount_dir)
-        mount = 'sudo mount -U %s %s' % (self.uuid, self.mount_dir)
-        print 'Mounting Device: %s (%s)' % (self.label, self.mount_dir)
-
+        mount = 'sudo mount -t %s /dev/sda1 %s' % (self.fstype, self.mount_dir)
+        print 'Mounting Device: %s (%s)' % (self.label, self.mount_dir)                   
+                                                  
         if not os.path.exists(self.mount_dir):
-            
             result = commands.getstatusoutput(mkdir)
 
             if result[0] != 0:
+                GPIO.output("P8_14", GPIO.LOW)
+                GPIO.output("P8_18", GPIO.HIGH)
                 print 'dir creation failed, aborting USB mount.'
+                time.sleep(2)
 
             else:
                 result = commands.getstatusoutput(mount)
                 self.mounted = True
 
                 if random.randint(1, 4) > 1:
-                    cp = "sudo sed -n 1p payload.txt > /media/"+self.label+"/payload.txt"
-                    result = commands.getstatusoutput(cp)
+                    print "egg"
+                    send = "sudo sed -n 1p eggload.txt > /media/"+self.label+"/eggload.txt"
+                    result = commands.getstatusoutput(send)
+                    
+                    if result[0] != 0:
+                        GPIO.output("P8_14", GPIO.LOW)
+                        GPIO.output("P8_18", GPIO.HIGH)
+                        print 'Failed to copy egg...'
+                        time.sleep(2)
+
+                    else:
+                        copy = "sudo sed -n 1p eggload.txt >> /USBDetector/usedegg.txt"
+                        result = commands.getstatusoutput(copy)
+                        delete = "sudo sed -i 1d eggload.txt"
+                        result = commands.getstatusoutput(delete)
 
                 else:
                     line = random.choice(open('badluck.txt').readlines())
-                    cp = "sudo echo "+line+" > /media/"+self.label+"/payload.txt"
-                    result = commands.getstatusoutput(cp)
+                    noegg = line.strip('\n')
+                    print noegg
+                    send = "sudo echo "+noegg+" > /media/"+self.label+"/eggload.txt"
+                    result = commands.getstatusoutput(send)
                     
         else:
+            GPIO.output("P8_14", GPIO.LOW)
+            GPIO.output("P8_18", GPIO.HIGH)
             print 'Unclean mount directory, please clean up.'
+            time.sleep(2)
 
-        if result[0] != 0:
-            print 'Failed to copy egg...'
-
-        else:
-            dl = "sudo sed -i 1d payload.txt"
-            result = commands.getstatusoutput(dl)
-
-        result = commands.getstatusoutput('sudo umount -f %s' % (self.mount_dir))
+        result = commands.getstatusoutput('sudo umount -f /dev/sda1')
         result = commands.getstatusoutput('sudo rm -rf %s' % (self.mount_dir))
-        time.sleep(2)
+        
+        time.sleep(1)
+        
+        GPIO.output("P8_14", GPIO.LOW)
+        GPIO.output("P8_18", GPIO.LOW)
+        GPIO.output("P8_10", GPIO.HIGH)
+        time.sleep(.25)
+        GPIO.output("P8_10", GPIO.LOW)
+        time.sleep(.25)
+        GPIO.output("P8_10", GPIO.HIGH)
         print "Jobs Done."
         
 if __name__ == '__main__':
